2d86b076a4fd83d84f29233003dee11acf2efbbe
Usinarock71 <uzzipiedhorrorworld@gmail.com>
2025-02-04 03:07:57 -0300

    amigo-secreto-v3

--- app.js
+++ app.js
@@ -2,22 +2,39 @@ let listaAmigos = []
 let amigoSecreto = ""
 let contador = listaAmigos.length-1
 
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
 function agregarAmigo() {
     let amigoDeUsuario = (document.getElementById('amigo').value);
+    let amigoUsuario = document.createElement("li")
+    amigoUsuario.textContent = amigoDeUsuario
 
     if (amigoDeUsuario == "") {
         alert("Porfavor, inserte algun nombre.")
     }
     else {
         listaAmigos.push(amigoDeUsuario)
+        document.getElementById("listaAmigos").appendChild(amigoUsuario)
         limpiarCaja()
     }
 }
 
+
 function sortearAmigo() {
     let resultado = Math.floor(Math.random() * listaAmigos.length)
-    console.log(resultado)
-    alert(listaAmigos[resultado])
+
+    if (listaAmigos.length < 0) {
+        alert("Porfavor, inserte al menos un nombre primero")
+    } 
+    else {
+        console.log(resultado)
+        alert(listaAmigos[resultado])
+        asignarTextoElemento("resultado", listaAmigos[resultado]);
+    }
 }
 
 function limpiarCaja() {
